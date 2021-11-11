0ed201dd4d578ed6e76043abf53d11995253ba3b
Andres Fonseca <andresmfonseca@outlook.com>
2021-11-11 10:22:54 -0500

    Requirimientos iniciales

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request, redirect, session
-import db
+from flask import Flask, render_template, request, redirect, session, flash
+from flask_session import Session
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash
+import db, secrets, os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(20)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['UPLOAD_FOLDER'] = './static/images/upload'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 @app.route('/')
 def login():
---
+++ db.py
@@ -0,0 +1,7 @@
+import datetime
+import json
+import random
+import sqlite3
+import string
+from flask.helpers import flash
+from werkzeug.security import generate_password_hash, check_password_hash
