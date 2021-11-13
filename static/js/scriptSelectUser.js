b24f201aaca87ec87a11c3a417b5a52bf8d1f530
THypnosT <zunigaw@uninorte.edu.co>
2021-11-13 17:06:16 -0500

    usuarios

--- static/js/scriptSelectUser.js
+++ static/js/scriptSelectUser.js
@@ -3,10 +3,34 @@ const selectedUsuario = document.querySelector(".selected-usuario");
 const optionsContainerUsuario = document.querySelector(".options-container-usuario");
 const optionsListUsuario = document.querySelectorAll(".option-user");
 
+const selectedSexCargo = document.querySelector(".selected-sc");
+const optionsContainerSexCargo = document.querySelector(".options-container-sc");
+const optionsListSexCargo = document.querySelectorAll(".option-sc");
+
+const selectedCargo = document.querySelector(".selected-cargo");
+const optionsContainerCargo = document.querySelector(".options-container-cargo");
+const optionsListCargo = document.querySelectorAll(".option-cargo");
+
+
+
 
 selectedUsuario.addEventListener("click", () => {
     if(sessionStorage.getItem('userType')=="superAdmin"){
         optionsContainerUsuario.classList.toggle("active");
+    }
+
+});
+
+selectedSexCargo.addEventListener("click", () => {
+    if(sessionStorage.getItem('userType')=="superAdmin"){
+        optionsContainerSexCargo.classList.toggle("active");
+    }
+
+});
+
+selectedCargo.addEventListener("click", () => {
+    if(sessionStorage.getItem('userType')=="superAdmin"){
+        optionsContainerCargo.classList.toggle("active");
     }
 
 });
@@ -20,3 +44,25 @@ optionsListUsuario.forEach(o => {
         
     });
 });
+
+optionsListSexCargo.forEach(o => {
+    o.addEventListener("click", () => {
+        selectedSexCargo.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainerSexCargo.classList.remove("active");
+        var nombre = document.querySelector(".selected-sc").textContent;
+        document.getElementById("inputSelectedSexo").value=nombre;
+        
+    });
+});
+
+optionsListCargo.forEach(o => {
+    o.addEventListener("click", () => {
+        selectedCargo.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainerCargo.classList.remove("active");
+        var nombre = document.querySelector(".selected-cargo").textContent;
+        document.getElementById("inputSelectedCargo").value=nombre;
+        
+    });
+});
+
+
